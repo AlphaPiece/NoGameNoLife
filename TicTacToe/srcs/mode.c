81e94db288db49cac54bb4270f460536bf3f2259
Zexi Wang <twopieces0921@gmail.com>
2019-04-08 20:23:47 -0400

    fixed leaks

--- TicTacToe/srcs/mode.c
+++ TicTacToe/srcs/mode.c
@@ -6,7 +6,7 @@
 /*   By: Zexi Wang <twopieces0921@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 09:55:57 by Zexi Wang         #+#    #+#             */
-/*   Updated: 2019/04/08 13:01:53 by Zexi Wang        ###   ########.fr       */
+/*   Updated: 2019/04/08 20:21:20 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,20 @@ void	human_mode(char board[EDGE][EDGE], char player)
 
 	while (true)
 	{
-		ft_printf("Place (row, column):\n");
+		ft_printf("Place (row, column): ");
 		ft_nextline(0, &buf);
 		coor = ft_strsplit(buf, " \t\n,()");
+		free(buf);
 		if (ft_strarrlen((const char **)coor) < 2)
 		{
-			free(buf);
 			ft_strarrdel(coor);
 			continue ;
 		}
 		r = ft_atoi(coor[0]) - 1;
 		c = ft_atoi(coor[1]) - 1;
+		ft_strarrdel(coor);
 		if (is_valid_place(board, r, c))
 		{
-			free(buf);
-			ft_strarrdel(coor);
 			board[r][c] = player;
 			break ;
 		}
@@ -61,5 +60,6 @@ void	computer_mode(char board[EDGE][EDGE], char player)
 				}
 			}
 	board[best_pos[0]][best_pos[1]] = player;
-	ft_printf("Place (row, column):\n(%d, %d)\n\n", best_pos[0], best_pos[1]);
+	ft_printf("Place (row, column): (%d, %d)\n\n",
+				best_pos[0] + 1, best_pos[1] + 1);
 }
